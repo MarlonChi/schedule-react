e873930cf6bab5face103a1a9f91f884c1d71025
MarlonChi <marlonchiodelli@hotmail.com>
2024-03-27 22:41:14 -0300

    feat: home base

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { Heading, Text } from "@ignite-ui/react";
+
+import PreviewImage from "../../assets/app-preview.png";
+
+import { Container, Hero, Preview } from "./styles";
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="3xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="lg">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={PreviewImage}
+          alt="Calendário simbolizando aplicação em funcionamento"
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  );
+}
